b64a93c0563b624f06b6f2f8512e899bf2855724
Alexander Hanschke <alexander.hanschke@gmail.com>
2015-04-26 19:20:59 +0200

    add license info and some more javadoc

--- src/main/java/org/springframework/social/wunderlist/api/Wunderlist.java
+++ src/main/java/org/springframework/social/wunderlist/api/Wunderlist.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.wunderlist.api;
 
 import org.springframework.social.ApiBinding;
@@ -9,14 +24,17 @@ import org.springframework.web.client.RestOperations;
 public interface Wunderlist extends ApiBinding {
 
     /**
+     * Returns the portion of the Wunderlist API containing the user operations.
      *
-     * @return
+     * @return the user operations
      */
     UserOperations userOperations();
 
     /**
+     * Returns the underlying {@link RestOperations} object allowing for consumption of Wunderlist endpoints that may not be otherwise covered by the API binding.
+     * The RestOperations object returned is configured to include an OAuth "Authorization" header on all requests.
      *
-     * @return
+     * @return the REST operations
      */
     RestOperations restOperations();
 
