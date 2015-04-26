b2dd147ba79e9d2476269ffd7ff864d357f73d47
Alexander Hanschke <alexander.hanschke@gmail.com>
2015-04-26 18:14:11 +0200

    use hook to configure json converter

--- src/main/java/org/springframework/social/wunderlist/api/impl/WunderlistTemplate.java
+++ src/main/java/org/springframework/social/wunderlist/api/impl/WunderlistTemplate.java
@@ -1,15 +1,11 @@
 package org.springframework.social.wunderlist.api.impl;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
 import org.springframework.social.wunderlist.api.UserOperations;
 import org.springframework.social.wunderlist.api.Wunderlist;
 import org.springframework.web.client.RestOperations;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Alexander Hanschke
@@ -28,14 +24,7 @@ public class WunderlistTemplate extends AbstractOAuth2ApiBinding implements Wund
     }
 
     @Override
-    protected List<HttpMessageConverter<?>> getMessageConverters() {
-        final List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
-        converters.add(jsonMessageConverter());
-
-        return converters;
-    }
-
-    private MappingJackson2HttpMessageConverter jsonMessageConverter() {
+    protected MappingJackson2HttpMessageConverter getJsonMessageConverter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
